8c8cc915fac10155d5e27850c3fc86e23d247e56
guptavicky595 <guptavicky595@gmail.com>
2020-05-28 13:42:59 +0530

    First Commit

---
+++ spring-boot-example/src/main/java/com/javatpoint/springbootexample/SpringBootExampleApplication.java
@@ -0,0 +1,14 @@
+package com.javatpoint.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+		System.out.println(" Welcome Vicky Gupta to Spring Boot application");
+	}
+
+}
